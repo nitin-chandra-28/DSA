aa8291c242273c3287dad3b4efc0d1dd6e0c3bff
Nitin Chandra <nc28072006@gmail.com>
2026-09-15 15:09:17 +0530

    Time: 0 ms (100%), Space: 42.1 MB (36.09%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solveMem(int n, int[] dp) {
+         if(n == 0){
+            return 1;
+        }
+        if(n == 1) return 1;
+
+        if(dp[n] != -1) return dp[n];
+
+        dp[n] = solveMem(n - 1, dp) + solveMem(n - 2, dp);
+        return dp[n];
+    }
+    public int climbStairs(int n) {
+       int[] dp = new int[n + 1];
+       Arrays.fill(dp, -1);
+       return solveMem(n, dp);
+    }
+}
