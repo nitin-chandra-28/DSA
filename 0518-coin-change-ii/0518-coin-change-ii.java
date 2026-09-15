ff9a92f5f47cb7d8617aa0990932b82b788041dd
Nitin Chandra <nc28072006@gmail.com>
2026-09-15 14:00:26 +0530

    Time: 13 ms (44.52%), Space: 56.6 MB (47.12%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,24 +1,31 @@
+import java.util.*;
+
 class Solution {
-    public int solveMem(int[] coins, int n, int s, int[][] dp) {
-        if(s == 0) {
-            return 1;
-        }
-        if(s < 0 || n == 0) {
-            return 0;
+    public int solveTab(int[] coins, int n, int s) {
+
+        int[][] dp = new int[n + 1][s + 1];
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 1;
         }
 
-        if (dp[n][s] != -1) return dp[n][s];
-        dp[n][s] = solveMem(coins, n - 1, s, dp) + solveMem(coins, n, s - coins[n - 1], dp);
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j <= s; j++) {
+                dp[i][j] = dp[i - 1][j];
+                if (coins[i - 1] <= j) {
+                    dp[i][j] += dp[i][j - coins[i - 1]];
+                }
+            }
+        }
 
         return dp[n][s];
-
     }
+
+
     public int change(int amount, int[] coins) {
+
         int n = coins.length;
-        int[][] dp = new int[n + 1][amount + 1];
-        for(int[] rows : dp) {
-            Arrays.fill(rows, -1);
-        }
-        return solveMem(coins, n, amount, dp);
+
+        return solveTab(coins, n, amount);
     }
 }
